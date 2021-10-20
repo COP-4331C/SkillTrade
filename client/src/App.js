e88e32c1b43f7ef2da5f99e20eb2931b8ccb06b1
RacsonTech <58871762+RacsonTech@users.noreply.github.com>
2021-10-20 01:57:17 -0400

    Added Landing Page Navigation Bar
    Added AppNavBar.js file that contains the logo, a sing in button, and sign un button.

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Login from './components/Login';
 import Registration from './components/registration/Registration';
 import Footer from './components/Footer';
+import AppNavBar from './components/AppNavBar';
 
 function App() {
   return (
 
     <div className="App">
-      {/*<Login />*/}
       {/* <Registration />*/}
+      <AppNavBar />
       <Login />
       <Footer />
     </div>

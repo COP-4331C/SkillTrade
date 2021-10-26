1c03671876ca225c47e7e5506023494d0907509d
WeiyuanW <66765006+WeiyuanW@users.noreply.github.com>
2021-10-26 01:59:17 -0400

    updated mobile UI

---
+++ mobile/screens/SignInScreen.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { 
+    View, 
+    Text, 
+    Button, 
+    StyleSheet 
+} from 'react-native';
+
+const SignInScreen = () => {
+    return (
+        <View style={Styles.container}>
+            <Text>SignInScreen</Text>
+            <Button
+                title="Click Here"
+                onPress={() => alert('Button Clicked!')}
+            />
+        </View>
+    )
+}
+
+export default SignInScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+})
+
+
---
+++ mobile/screens/SignUpScreen.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { 
+    View, 
+    Text, 
+    Button, 
+    StyleSheet 
+} from 'react-native';
+
+const SignUpScreen = () => {
+    return (
+        <View style={Styles.container}>
+            <Text>SignUpScreen</Text>
+            <Button
+                title="Click Here"
+                onPress={() => alert('Button Clicked!')}
+            />
+        </View>
+    )
+}
+
+export default SignUpScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+})
+
+

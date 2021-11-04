c39e039064228e440cdb055c43e4082d489a66bf
RacsonTech <58871762+RacsonTech@users.noreply.github.com>
2021-11-04 19:04:44 -0400

    Removed M.I., Added edit button

--- client/src/Pages/ProfilePage.js
+++ client/src/Pages/ProfilePage.js
@@ -24,6 +24,7 @@ import EditProfile from '../components/EditProfile';
 import { Box, grid } from '@mui/system';
 import whoa from '../whoa.jpeg';
 import ForumOutlinedIcon from '@mui/icons-material/ForumOutlined';
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 
 export default function ProfilePage() {
 
@@ -41,47 +42,71 @@ export default function ProfilePage() {
         
 
       {/* //1ST component paper// */}
-      <Paper variant="outlined" elevation={3} style={{
-          position: 'left', 
+      <Paper
+        variant="outlined"
+        elevation={3}
+        style={{
+          position: 'left',
           borderColor: "black",
-          justifyContent: 'center', 
-          alignItems: 'center', 
-          padding: 10, 
-          height: '25vh', 
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 10,
+          height: '25vh',
           width: 500,
           backgroundColor: 'lightBlue',
           borderRadius:20,
           borderWidth: 2,
           marginTop: 50,
-          marginLeft: 20 }}>
+          marginLeft: 20
+        }}
+      >
+        <Grid container direction="row" spacing={3} justifyContent="center" alignItems="flex-start">
+          <Grid item xs>
 
-          {/* //Profile Image// */}
-          
+          </Grid>
+          <Grid item xs>
+            {/********************* Profile Image *********************************/}
             <Box sx={{ display: 'flex', justifyContent: 'center' }}>
               {/* //is this for image form label */}
                   {/* <div>
                     <input type="file" onChange={this.handleChange}/>
                     <img src={this.state.file}/>
                   </div> */}
-              <img  style={{ 
-                marginTop: -50, 
-                width: 70, 
-                height: 70, 
+              <img  style={{
+                marginTop: -50,
+                width: 70,
+                height: 70,
                 borderRadius: 200 / 2,
-                border: "2px solid", 
-                borderColor: 'black' }} 
+                border: "2px solid",
+                borderColor: 'black' }}
                 src={whoa}/>
             </Box>
+          </Grid>
+          <Grid item xs>
+            {/********************* Edit Button *********************************/}
+            {/*<Box sx={{flexGrow: 1, textAlign: "right"}}>*/}
+            <Box sx={{textAlign: "right"}}>
+              <IconButton
+                color="primary"
+                aria-label="edit"
+                // onClick={handleOpen}
+              >
+                <EditOutlinedIcon/>
+              </IconButton>
+            </Box>
+          </Grid>
 
+
+        </Grid>
           <Grid container spacing={4} justifyContent="center" paddingTop={2}>
 
-            <Grid item xs={4}>
+            <Grid item xs={6}>
               <FormControl>
               <item>
               <TextField
                 id="outlined-read-only-input"
                 label="First Name"
-                defaultValue="Hello World"
+                defaultValue="John"
                 InputProps={{
                 readOnly: true,
                 }}
@@ -90,28 +115,28 @@ export default function ProfilePage() {
               </FormControl>
             </Grid>
 
-            <Grid item xs={2}>
-              <FormControl>
-              <item>
-                <TextField
-                  id="outlined-read-only-input"
-                  label="M.I."
-                  defaultValue="Hello World"
-                  InputProps={{
-                  readOnly: true,
-                  }}
-                />
-                  </item>
-              </FormControl>
-            </Grid>
+            {/*<Grid item xs={2}>*/}
+              {/*<FormControl>*/}
+              {/*<item>*/}
+              {/*  <TextField*/}
+              {/*    id="outlined-read-only-input"*/}
+              {/*    label="M.I."*/}
+              {/*    defaultValue="Hello World"*/}
+              {/*    InputProps={{*/}
+              {/*    readOnly: true,*/}
+              {/*    }}*/}
+              {/*  />*/}
+              {/*    </item>*/}
+              {/*</FormControl>*/}
+            {/*</Grid>*/}
 
-            <Grid item xs={4}>
+            <Grid item xs={6}>
               <FormControl>
               <item>
                 <TextField
                   id="outlined-read-only-input"
                   label="Last Name"
-                  defaultValue="Hello World"
+                  defaultValue="Doe"
                   InputProps={{
                   readOnly: true,
                   }}
@@ -128,7 +153,7 @@ export default function ProfilePage() {
                 type='submit'
                 color='primary'
                 variant='contained'
-                style={{ marginTop:50 }}
+                style={{ marginTop:40 }}
                 startIcon={<ForumOutlinedIcon />}
               >
                 Contact Me!

52c881944c07dfe3a58f954616df3ef6cc819838
Ster <sterz.dev@gmail.com>
2021-11-05 16:46:43 -0400

    Implement Conversation/Messaging API (No Authentication/Validation Yet)

---
+++ server/controllers/message.controller.js
@@ -0,0 +1,36 @@
+const Message = require("../models/message.model");
+
+exports.create = async (req, res) => {
+  const message = new Message(req.body);
+
+  message
+    .save()
+    .then((savedMessage) => {
+      return res.status(200).json(savedMessage);
+    })
+    .catch((err) => {
+      console.log("An error occured.");
+      console.log(err);
+      return res.status(500).json(err);
+    });
+};
+
+exports.fetchConversationMessages = async (req, res) => {
+  Message.find({ conversationId: req.params.conversationId })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+};
+
+exports.deleteMessage = async (req, res) => {
+  Message.deleteOne({ messageId: req.params.messageId })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+};
---
+++ server/routes/conversation.routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const conversations = require("../controllers/conversation.controller");
+
+const authenticateToken = require("../middleware/auth.js");
+
+router.post("/", conversations.create);
+
+router.get("/:userId", conversations.fetchUserConversations);
+
+module.exports = router;

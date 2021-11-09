98fb0fa87a3ef9b05bd420a95fc36181ba69fe91
RafaelVCS <46545550+RafaelVCS@users.noreply.github.com>
2021-11-09 12:38:26 -0500

    cleaned up "add review" code

--- server/controllers/review.controller.js
+++ server/controllers/review.controller.js
@@ -5,50 +5,36 @@ const Review = require("../models/review.model");
 exports.createReview = async (req, res) => {
 
     const {rating, subjectId} = req.body;
-    
 
+    // Validate that subjectId is for a real user
     try{
-        var subject = await User.findOne({ _id: subjectId });
+        let subject = await User.findOne({ _id: subjectId });
+        if (!subject)
+            return res.status(400).json({ error: "not a valid user to review." });
     }
     catch(err){
         console.log(err);
         return res.status(500).json({ error: err });
-    }
-    
-    if (!subject)
-    {
-        return res.status(400).json({ error: "not a valid user to review." });
-    }
-    else
-        console.log("found subject: %s", subject.email);
+    } 
 
+    // Grab objectId value from author (should be available and valid via authenticationToken)
     try{
         var author = await User.findOne({ email: req.email});
+        if(!author)
+            return res.status(400).json({ error: "could not obtain userId" });
     }
     catch(err){
         console.log(err);
         return res.status(500).json({error: err});
     }
 
-    if(!author)
-    {
-        return res.status(400).json({ error: "this should be a valid user!!!" });
-    }
-    else
-    {
-        console.log("found author: %s", author.email);
-
-    }
-
-
-
+    // Make sure rating is a valid number between 1 and 5
     if(!Number.isInteger(rating) || rating < 1 || rating > 5)
-    {
         return res.status(400).json({ error: "invalid rating: must be an INTEGER from 1-5." });
-    }
 
+    // Add author ObjectId to request body, then create and add new review to database.
+    req.body.authorId = author._id;
     const review = new Review(req.body);
-    review.authorId = author._id;
     review.save()
         .then(() => {
         return res.status(200).json({ message: "Successfully created review!" });
@@ -58,5 +44,4 @@ exports.createReview = async (req, res) => {
         console.log(err);
         return res.status(400).json({ error: err });
         });
-
 }

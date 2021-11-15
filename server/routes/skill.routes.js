143907b9c3f641e85074d7dfc5cc1ffbdb9f507c
Ster <sterz.dev@gmail.com>
2021-11-15 12:00:46 -0500

    Fixes for errors when testing skills API

--- server/routes/skill.routes.js
+++ server/routes/skill.routes.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const skill = require("../controllers/skill.controller");
-const authenticateToken = require('../middleware/auth');
+const authenticateToken = require("../middleware/auth");
 
-
-router.route("/")
+router
+  .route("/")
   .get(skill.search) // Fetch by page (query parameter)
-  .post(skill.createSkill) // Add skill
-  
-router.route("/:skillId")
+  .post(skill.createSkill); // Add skill
+
+router
+  .route("/:skillId")
   .get(skill.fetchOne) // Get single skill
   .delete(authenticateToken, skill.deleteSkill) // Delete skill
-  .put(skill.editSkill) // Edit skill
+  .put(skill.editSkill); // Edit skill
 
 module.exports = router;

b85177685a4f787bdbb275885b27898b274ce057
Ster <sterz.dev@gmail.com>
2021-11-16 19:10:26 -0500

    Merge branch 'main' of https://github.com/COP-4331C/Large-Project

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const users = require("../controllers/user.controller");
-const authenticateToken = require('../middleware/auth');
+const authenticateToken = require("../middleware/auth");
 
 // Register
 router.post("/register", users.create);
 
 // Edit Profile
-router.patch("/edit-profile", authenticateToken, users.editProfile);
+router.put("/edit-profile", authenticateToken, users.editProfile);
 
 // Change Password
 router.patch("/change-password", authenticateToken, users.changePassword);
